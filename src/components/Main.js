4d944d582baa56a3fae08231257eabf129503069
artem <artemstrpcv@gmail.com>
2019-10-07 21:39:42 +0700

    init

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Test} from './test/Test'
+
+export class Main extends Component {
+    render() {
+        return (
+            <>
+                <Test/>
+            </>
+        );
+    }
+}
+
+export default Main;
