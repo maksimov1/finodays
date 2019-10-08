f547072e5789564307a4103c43bce74a0db12f4f
Alexey Kozyakov <alex-kozyakov@yandex-team.ru>
2019-10-08 05:59:49 +0300

    add monthly payments modal

--- src/components/modals/monthlyPaymentsModal/MonthlyPaymentsModal.js
+++ src/components/modals/monthlyPaymentsModal/MonthlyPaymentsModal.js
@@ -4,6 +4,10 @@ import './MonthlyPaymentsModal.css';
 export class MonthlyPaymentsModal extends Component {
     constructor(props) {
         super(props);
+        this.works = [
+            { id: 1, text: 'Task #1', start_date: '2019-04-15', duration: 3, progress: 0.6, price: 228 },
+            { id: 2, text: 'Task #2', start_date: '2019-04-18', duration: 3, progress: 0.4, price: 1488 }
+          ];
         //data expected in props.works
         //format: [
         //     { id: 1, text: 'Task #1', start_date: '2019-04-15', duration: 3, progress: 0.6, price: 228 },
@@ -13,11 +17,11 @@ export class MonthlyPaymentsModal extends Component {
 
 
     getEndMonth(work) {
-        return (parseInt(work.start_date.split('-')[1]) + work.duration) % 12 + 1;
+        return parseInt(work.start_date.split('-')[1]) + work.duration;
     }
 
     getWorkByEndMonth(endMonth) {
-        return this.props.works.filter(work => this.getEndMonth(work) === endMonth)
+        return this.works.filter(work => this.getEndMonth(work) === endMonth)
             .reduce((accumulator, currentValue) => accumulator + currentValue.price, 0);
     }
 
@@ -31,26 +35,28 @@ export class MonthlyPaymentsModal extends Component {
 
     render() {
         return (
-            <table>
-                <caption>Помесячные платы</caption>
-                <tr>
-                    <th>Январь</th>
-                    <th>Февраль</th>
-                    <th>Март</th>
-                    <th>Апрель</th>
-                    <th>Май</th>
-                    <th>Июнь</th>
-                    <th>Июль</th>
-                    <th>Август</th>
-                    <th>Сентябрь</th>
-                    <th>Октябрь</th>
-                    <th>Ноябрь</th>
-                    <th>Декабрь</th>
-                </tr>
-                <tr>
-                    {this.createTableContent().map(sum => <td>{sum}</td>)}
-                </tr>
-            </table>
+            <div className="monthly_payments_modal">
+                <div className="monthly_payments_modal_wrapper">
+                    <div className="table_header">Помесячные платы</div>
+                    <div className="table_title_row">
+                        <div>Январь</div>
+                        <div>Февраль</div>
+                        <div>Март</div>
+                        <div>Апрель</div>
+                        <div>Май</div>
+                        <div>Июнь</div>
+                        <div>Июль</div>
+                        <div>Август</div>
+                        <div>Сентябрь</div>
+                        <div>Октябрь</div>
+                        <div>Ноябрь</div>
+                        <div>Декабрь</div>
+                    </div>
+                    <div className="table_content">
+                        {this.createTableContent().map(sum => <div>{sum}</div>)}
+                    </div>
+                </div>
+            </div>
         );
     }
 }
